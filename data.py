423ec8faceae6fef7ed3eb5f006a0191b1fe6c7d
WywySenarios <wywyericzhu@gmail.com>
2026-02-23 11:18:18 -0500

    NotRequired & DictInfo
    * Added NotRequired to some keys
    * Added Dictionary versions of the types.

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-from typing import Any, Literal, TypeAlias, TypedDict
+from typing import Any, Literal, NotRequired, TypeAlias, TypedDict
 
 Datatype: TypeAlias = Literal["int", "integer", "float", "number", "string", "str", "text", "bool", "boolean", "date", "time", "timestamp", "enum", "geodetic point"]
 PostgresDatatype: TypeAlias = Literal["integer", "real", "double precision", "text", "boolean", "date", "time", "timestamp", "interval", "enum", "ST_Point"]
@@ -10,13 +10,20 @@ class DataColumn(TypedDict):
     defaultValue: Any
     entrytype: str
     invalidInputMessage: str | None
-    comments: bool | None
+    comments: NotRequired[bool]
     description: str | None
     unique: bool | None
 
+Schema: TypeAlias = list[DataColumn]
+DictSchema: TypeAlias = dict[str, DataColumn]
+
 class DescriptorInfo(TypedDict):
     name: str
-    schema: list[DataColumn]
+    schema: Schema
+
+class DictDescriptorInfo(TypedDict):
+    name: str
+    schema: DictSchema
 
 class TableInfo(TypedDict):
     tableName: str
@@ -24,12 +31,33 @@ class TableInfo(TypedDict):
     read: bool
     write: bool
     comments: bool
-    schema: list[DataColumn]
-    descriptors: list[DescriptorInfo]
+    tagging: NotRequired[bool]
+    schema: Schema
+    descriptors: NotRequired[list[DescriptorInfo]]
+    
+class DictTableInfo(TypedDict):
+    tableName: str
+    entrytype: Literal["form", "timer"]
+    read: bool
+    write: bool
+    comments: bool
+    tagging: NotRequired[bool]
+    schema: DictSchema
+    descriptors: NotRequired[dict[str, DictDescriptorInfo]]
 
 class DatabaseInfo(TypedDict):
     dbname: str
     tables: list[TableInfo]
 
+DictDatabaseInfo: TypeAlias = dict[str, DictTableInfo]
+
 class MainConfig(TypedDict):
     data: list[DatabaseInfo]
+
+# User input
+EntryData: TypeAlias = dict[str, Any]
+Entry: TypeAlias = dict[Literal["data", "descriptors", "tags"], EntryData]
+
+class EntryTableData(TypedDict):
+    columns: list[str]
+    data: list[list[Any]]
